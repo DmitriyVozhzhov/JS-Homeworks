fb5b96cd657653c583b68974f3a2b74d2f969721
Dmitriy Vozhzhov <Dmitriy.vozhzhov@gmail.com>
2022-01-13 17:16:50 +0200

    create hw-3

---
+++ hw 3/hw3.js
@@ -0,0 +1,11 @@
+// task 1
+let arr = [1, 2, 3, 'a', 'b', 'c', '4', '5', '6'];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+if (isNaN(ar=== true){
+  sum = sum + parseInt(arr[i]);
+} else { 
+break
+}
+}
+alert(sum);
