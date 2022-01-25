446e29d095f4c13f11c2f6480341403d6679b82c
Dmitriy <74067745+DmitriyVozhzhov@users.noreply.github.com>
2022-01-25 23:42:45 +0200

    hw 3 update

--- hw 3/hw3.js
+++ hw 3/hw3.js
@@ -1,11 +1,21 @@
 // task 1
 let arr = [1, 2, 3, 'a', 'b', 'c', '4', '5', '6'];
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-if (isNaN(ar=== true){
-  sum = sum + parseInt(arr[i]);
-} else { 
-break
-}
-}
+let sum = arr.reduce((acc, item) => {
+	if (!isNaN(+item)) return acc + +item;
+	return acc;
+}, 0);
+
 alert(sum);
+
+// task 2
+let arr = [];
+let n = (Math.round(Math.random() * 10))
+for (let i = 0; i < n; i++) {
+	arr.push(Math.floor(Math.random() * 100) / 100);
+}
+console.log(arr)
+arr.forEach(function (item, i, arr) {
+	console.log(` ${item}` ** 5);
+});
+
+// task 3
