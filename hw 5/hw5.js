bcded1b38e0dca38cf253365dd1cc1531949e037
Dmitriy <74067745+DmitriyVozhzhov@users.noreply.github.com>
2022-03-10 15:36:26 +0200

    add hw 5

---
+++ hw 5/hw5.js
@@ -0,0 +1,174 @@
+const objects = [
+	{
+			id: 1,
+			displayName: 'Object John',
+			date: '12-10-2020',
+			enabled: true,
+			relation: null
+	},
+	{
+			id: 2,
+			displayName: 'Object Kate',
+			date: '06-21-2020',
+			enabled: false,
+			relation: {
+					relationId: 1
+			}
+	},
+	{
+			id: 3,
+			displayName: 'Object Bob',
+			date: '04-03-2020',
+			enabled: true,
+			relation: {
+					relationId: 1
+			}
+	},
+	{
+			id: 4,
+			displayName: 'Object Alex',
+			date: '12-12-2021',
+			enabled: false,
+			relation: null
+	},
+	{
+			id: 5,
+			displayName: 'Object Pooja',
+			date: '07-30-2019',
+			enabled: true,
+			relation: {
+					relationId: 4
+			}
+	},
+	{
+			id: 6,
+			displayName: 'Object Scott',
+			date: '02-15-2021',
+			enabled: true,
+			relation: {
+					relationId: 2
+			}
+	},
+	{
+			id: 7,
+			displayName: 'Object Sergey',
+			date: '01-06-2020',
+			enabled: true,
+			relation: {
+					relationId: 5
+			}
+	},
+	{
+			id: 8,
+			displayName: 'Object Adhey',
+			date: '03-21-2021',
+			enabled: true,
+			relation: {
+					relationId: 5
+			}
+	},
+	{
+			id: 9,
+			displayName: 'Object Travis',
+			date: '08-10-2021',
+			enabled: true,
+			relation: {
+					relationId: 1
+			}
+	},
+	{
+			id: 10,
+			displayName: 'Object Artem',
+			date: '09-24-2021',
+			enabled: false,
+			relation: {
+					relationId: 4
+			}
+	},
+	{
+			id: 11,
+			displayName: 'Object Stuart',
+			date: '04-01-2020',
+			enabled: true,
+			relation: {
+					relationId: 7
+			}
+	},
+	{
+			id: 12,
+			displayName: 'Object Keith',
+			date: '05-08-2020',
+			enabled: true,
+			relation: {
+					relationId: 10
+			}
+	},
+	{
+			id: 13,
+			displayName: 'Object Bill',
+			date: '10-17-2020',
+			enabled: true,
+			relation: {
+					relationId: 9
+			}
+	},
+	{
+			id: 14,
+			displayName: 'Object Chuck',
+			date: '04-15-2020',
+			enabled: true,
+			relation: null
+	},
+	{
+			id: 15,
+			displayName: 'Object Joel',
+			date: '12-09-2020',
+			enabled: true,
+			relation: null
+	},
+	{
+			id: 16,
+			displayName: 'Object Tim',
+			date: '01-12-2020',
+			enabled: true,
+			relation: {
+					relationId: 15
+			}
+	},
+	{
+			id: 17,
+			displayName: 'Object Kevin',
+			date: '09-05-2020',
+			enabled: true,
+			relation: {
+					relationId: 7
+			}
+	},
+	{
+			id: 18,
+			displayName: 'Object Jessica',
+			date: '07-19-2021',
+			enabled: true,
+			relation: {
+					relationId: 12
+			}
+	},
+	{
+			id: 19,
+			displayName: 'Object Monica',
+			date: '02-08-2021',
+			enabled: true,
+			relation: {
+					relationId: 14
+			}
+	},
+	{
+			id: 20,
+			displayName: 'Object Tony',
+			date: '09-05-2020',
+			enabled: true,
+			relation: {
+					relationId: 7
+			}
+	}
+];
